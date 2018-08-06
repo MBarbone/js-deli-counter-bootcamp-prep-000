55780358f5e33ada89e06fd39f8e776337c321a2
Michael Barbone <michael.barbone@gmail.com>
2018-08-06 19:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+var katzDeliLine = [];
+
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
-
-
-
 
 
 function nowServing(katzDeli) {
@@ -23,13 +22,20 @@ function nowServing(katzDeli) {
 
 
 
+
+
 var line = [];
- function currentLine(katzDeli) {
+
+function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    line.push(` `+[i+1]+`. `  + katzDeli[i]);
   }
   if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
+    return ("The line is currently empty.");
+  } else {
+    return (`The line is currently:` + line);
+  }
 }
+
+
+
